addc755c8af47f56de9002cc968b4d96a82a252d
BatteryAcidDev <battery4cid@gmail.com>
2022-06-09 13:26:43 -0400

    Fixed name of API by adding .service
    - added form and supporting service calls to create and list todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from '../API.service';
+import { API } from 'aws-amplify';
+import * as queries from '../graphql/queries';
+import * as mutations from '../graphql/mutations';
+import { GraphQLResult } from '@aws-amplify/api-graphql';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ccdiscovery';
+    title = 'ccdiscovery';
+    public createForm: FormGroup;
+    public todos: Array<Todo> = [];
+
+    constructor(private fb: FormBuilder) {
+        this.createForm = this.fb.group({
+            name: ['', Validators.required],
+            description: ['', Validators.required]
+        });
+    }
+
+    public async onCreate(todo: Todo) {
+        console.log("Calling onCreate.");
+        let createTodoResponse = await API.graphql({
+            query: mutations.createTodo,
+            variables: { input: todo }
+        }) as GraphQLResult<any>;
+        console.log(createTodoResponse);
+        this.createForm.reset();
+        await this.listTodos();
+    }
+
+    async listTodos() {
+        let listTodoResponse = await API.graphql({
+            query: queries.listTodos
+        }) as GraphQLResult<any>;
+        console.log(listTodoResponse);
+        this.todos = listTodoResponse.data.listTodos.items as Todo[];
+    }
+
+    async ngOnInit() {
+        await this.listTodos();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AmplifyAuthenticatorModule
+    AmplifyAuthenticatorModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
