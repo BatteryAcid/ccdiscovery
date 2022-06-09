f05aea4c9763fc9d71dbf8618cafae96a404931a
BatteryAcidDev <battery4cid@gmail.com>
2022-06-09 14:12:45 -0400

    Added @auth to Todo model
    - added update form to view
    - API calls now specific auth mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { API } from 'aws-amplify';
 import * as queries from '../graphql/queries';
 import * as mutations from '../graphql/mutations';
 import { GraphQLResult } from '@aws-amplify/api-graphql';
+import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api-graphql';
 
 @Component({
     selector: 'app-root',
@@ -14,10 +15,17 @@ import { GraphQLResult } from '@aws-amplify/api-graphql';
 export class AppComponent {
     title = 'ccdiscovery';
     public createForm: FormGroup;
+    public updateForm: FormGroup;
     public todos: Array<Todo> = [];
 
     constructor(private fb: FormBuilder) {
         this.createForm = this.fb.group({
+            name: ['', Validators.required],
+            description: ['', Validators.required]
+        });
+
+        this.updateForm = this.fb.group({
+            id: ['', Validators.required],
             name: ['', Validators.required],
             description: ['', Validators.required]
         });
@@ -27,16 +35,34 @@ export class AppComponent {
         console.log("Calling onCreate.");
         let createTodoResponse = await API.graphql({
             query: mutations.createTodo,
-            variables: { input: todo }
+            variables: { input: todo },
+            authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
         }) as GraphQLResult<any>;
         console.log(createTodoResponse);
         this.createForm.reset();
         await this.listTodos();
     }
 
+    public async onUpdate(todo: Todo) {
+        console.log("Calling onUpdate.");
+        let updateTodoResponse = await API.graphql({
+            query: mutations.updateTodo,
+            variables: { input: todo },
+            authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+        }) as GraphQLResult<any>;
+        console.log(updateTodoResponse);
+        this.updateForm.reset();
+        await this.listTodos();
+    }
+
+    async populateUpdateForm(todo: Todo) {
+        this.updateForm.patchValue(todo);
+    }
+
     async listTodos() {
         let listTodoResponse = await API.graphql({
-            query: queries.listTodos
+            query: queries.listTodos,
+            authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
         }) as GraphQLResult<any>;
         console.log(listTodoResponse);
         this.todos = listTodoResponse.data.listTodos.items as Todo[];
